a90e0879907f9a7f2ccd288bb6307656953646c9
Ivan Rojo <ivanrojo07@gmail.com>
2018-03-20 16:00:27 -0600

    home page and working with register

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RegisterPage } from '../register/register';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +10,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  register(){
+    this.navCtrl.push(RegisterPage);
   }
 
 }
